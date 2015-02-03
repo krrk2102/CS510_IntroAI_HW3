7aafef19048c283dbeab3d664dc11a26a9e12bcb
krrk2102 <wushangqi@gmail.com>
2015-02-02 23:15:16 -0500

    Initial Commit

---
+++ AI_hw3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AI_hw3
+//
+//  Created by Shangqi Wu on 15/2/2.
+//  Copyright (c) 2015年 Shangqi Wu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
